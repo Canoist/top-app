2dd5e9cba86b2ee89e3a766eb7c8d1da29107710
Leo Lichkun (Canoist) <lionproklon@gmail.com>
2022-08-27 01:43:58 +0300

    Added stylelints, fixed css with rules at stylelintrc, added debugger

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home(): JSX.Element {
+    const author = "Canoist";
+
     return (
         <div className={styles.container}>
             <Head>
@@ -68,7 +70,7 @@ export default function Home(): JSX.Element {
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
                     rel="noopener noreferrer">
-                    Powered by{" "}
+                    Powered by{author}
                     <span className={styles.logo}>
                         <Image
                             src="/vercel.svg"
