b530d14ec9e73250bfe709b292ffe6268a3897ae
Leo Lichkun (Canoist) <lionproklon@gmail.com>
2022-09-03 02:18:13 +0300

    Added styles for sideBar

--- layouts/SideBar/SideBar.tsx
+++ layouts/SideBar/SideBar.tsx
@@ -1,14 +1,17 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from "react";
-// import styles from "./SideBar.module.css";
-// import cn from "classnames";
+import styles from "./SideBar.module.css";
+import cn from "classnames";
 import Menu from "../Menu/Menu";
+import Logo from "../logo.svg";
 
 interface ISideBar
     extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {}
 
-const SideBar: React.FC<ISideBar> = ({ ...props }) => {
+const SideBar: React.FC<ISideBar> = ({ className, ...props }) => {
     return (
-        <aside {...props}>
+        <aside className={cn(className, styles.sidebar)} {...props}>
+            <Logo className={styles.logo} />
+            <div>Search</div>
             <Menu />
         </aside>
     );
